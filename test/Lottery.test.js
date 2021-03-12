eb795335ccd4227f70d25058031328cbb5070b07
snowwhitedev <snowwhite91924@gmail.com>
2021-03-13 02:34:10 +0700

    feat: finish smart contract and test

---
+++ test/Lottery.test.js
@@ -0,0 +1,47 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let lottery;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts()
+
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there'] })
+    .send({ from: accounts[0], gas: '1000000' })
+});
+
+describe('Lottery Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(lottery.options.address);
+  });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter.send({
+      from: accounts[0],
+      value: web3.utilsweb3.toWei('0.02', 'ether') 
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
+  });
+
+  it('requires a minimum amount of ether to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0
+      });
+    } catch (err) {
+      assert(err);
+    }
+  });
+});
